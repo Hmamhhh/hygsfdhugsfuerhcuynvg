cdb1859ee989d59d18265a84ec2eaf11fdafb0d2
Hmamhhh <41455090+Hmamhhh@users.noreply.github.com>
2018-07-27 00:59:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -131,7 +131,7 @@ client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('b!bc')){
- if (message.author.id !== '389090790984515594') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if (message.author.id !== '406451228004974603') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
 message.channel.sendMessage('جار ارسال الرسالة |✅')
 client.users.forEach(m =>{
 m.sendMessage(args)
@@ -639,17 +639,7 @@ message.author.send(`https://discordapp.com/oauth2/authorize?client_id=${client.
   
 
 
-client.on('message', message => {
-            if(!message.channel.guild) return;
-let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('jjjjjjaaaaaayayayayyayatstyatsdtasytdutut')){
- if(!message.author.id === '406451228004974603') return;
-message.channel.sendMessage('جار ارسال الرسالة |✅')
-client.users.forEach(m =>{
-m.sendMessage(args)
-})
-}
-});
+
 
 
 
