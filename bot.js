74b1ce86b01e4e7d7f0b816bd0839bc9afb31695
Hmamhhh <41455090+Hmamhhh@users.noreply.github.com>
2018-07-27 21:14:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -41,75 +41,7 @@ client.user.setStatus("dnd");
 
 
 
-const fs = require("fs")
-let ar = JSON.parse(fs.readFileSync(`./Data/autorole.json`, `utf8`))
 
-const prefix = "b!";
-
-client.on('guildMemberAdd', member => {
-  if(!ar[member.guild.id]) ar[member.guild.id] = {
-  onoff: 'Off',
-  role: 'Member'
-  }
-  if(ar[member.guild.id].onoff === 'Off') return;
-member.addRole(member.guild.roles.find(`name`, ar[member.guild.id].role)).catch(console.error)
-})
-
-client.on('message', message => { 
-  var whitelisted = "406451228004974603"
-  var sender = message.author
-
-if(!message.guild) return
-  if(!ar[message.guild.id]) ar[message.guild.id] = {
-  onoff: 'Off',
-  role: 'Member'
-  }
-
-if(message.content.startsWith(prefix + `autorole`)) {
-         if(whitelisted.includes(sender.id)) {
-  let perms = message.member.hasPermission(`MANAGE_ROLES`)
-
-  if(!perms) return message.reply(`لا تملك صلاحية تعديل الرتب.`)
-  let args = message.content.split(" ").slice(1)
-  if(!args.join(" ")) return message.reply(`${prefix}autorle toggle/setrole [ROLE NAME]`)
-  let state = args[0]
-  if(!state.trim().toLowerCase() == 'toggle' || !state.trim().toLowerCase() == 'setrole') return message.reply(`Please type a right state, ${prefix}modlogs toggle/setrole [ROLE NAME]`) 
-    if(state.trim().toLowerCase() == 'toggle') { 
-     if(ar[message.guild.id].onoff === 'Off') return [message.channel.send(`**The Autorole Is __𝐎𝐍__ !**`), ar[message.guild.id].onoff = 'On']
-     if(ar[message.guild.id].onoff === 'On') return [message.channel.send(`**The Autorole Is __𝐎𝐅𝐅__ !**`), ar[message.guild.id].onoff = 'Off']
-    }
-   if(state.trim().toLowerCase() == 'set') {
-   let newRole = message.content.split(" ").slice(2).join(" ")
-   if(!newRole) return message.reply(`${prefix}autorole setrole [ROLE NAME]`)
-     if(!message.guild.roles.find(`name`,newRole)) return message.reply(`لا استطيع ايجاد هذه الرتبة.`)
-    ar[message.guild.id].role = newRole
-     message.channel.send(`**الرتبة التلقائية تم تعديلها الى${newRole}.**`)
-   } 
-         }
-  }
- 
-if(message.content === prefix + 'info') {
-    let perms = message.member.hasPermission(`MANAGE_GUILD`) 
-    if(!perms) return message.reply(`انت لا تمتلك الصلاحيات.`)
-    var embed = new Discord.RichEmbed()
-
-.addField(`Autorole : :sparkles:  `, `
-
-State : __${ar[message.guild.id].onoff}__
-Role : __${ar[message.guild.id].role}__`)
-
-
-    .setColor(`RANDOM`)
-    message.channel.send({embed})
-  }
-
-
-    fs.writeFile("./Data/AutoRole.json", JSON.stringify(ar), (err) => {
-    if (err) console.error(err)
-  });
-
-
-})
 
 
 
