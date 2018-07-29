87127d7341675f9d8e79b982e38dbf24199ea427
Hmamhhh <41455090+Hmamhhh@users.noreply.github.com>
2018-07-29 09:59:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -243,7 +243,6 @@ client.on('message', function(msg) {
 
 
 const db = require('quick.db');
-const client = new Discord.Client();   
 const giphy = require('giphy-api')();    
 const googl = require('goo.gl');  
 const translate = require('google-translate-api');   
